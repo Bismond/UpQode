1fcdd068ecca7a016252f6dd5f1759cf3c6a0d34
Dmytro Hrynchuk <dmytrohrynchuk@gmail.com>
2019-11-19 01:25:21 +0200

    latest commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,47 @@
+const locations = [
+    {
+        cityName: 'Los Angeles',
+        latitude: 34.0194,
+        longitude: -118.411
+    },
+    {
+        cityName: 'New York',
+        latitude: 40.6643,
+        longitude: -73.9385
+    },
+    {
+        cityName: 'Boston',
+        latitude: 42.332,
+        longitude: -71.0202
+    },
+    {
+        cityName: 'Detroit',
+        latitude: 42.383,
+        longitude: -83.1022
+    }
+];
+
 const tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 
 const firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-let player1, player2, player3;
+let players = [];
+
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
 
 function onYouTubeIframeAPIReady() {
-    player1 = new YT.Player('player1', {
+    players[0] = new YT.Player('player1', {
         playerVars: {
             'controls': 0,
             'fs': 0,
@@ -18,13 +52,14 @@ function onYouTubeIframeAPIReady() {
             'modestbranding': 1,
             'rel': 0,
             'showinfo': 0,
+            'origin': 'https://www.youtube.com'
         },
         videoId: 'W0LHTWG-UmQ',
         events: {
             'onReady': onPlayerReady
         }
     });
-    player2 = new YT.Player('player2', {
+    players[1] = new YT.Player('player2', {
         playerVars: {
             'controls': 0,
             'fs': 0,
@@ -34,14 +69,15 @@ function onYouTubeIframeAPIReady() {
             'playlist': 'X8Z8okhkjv8',
             'modestbranding': 1,
             'showinfo': 0,
-            'rel': 0
+            'rel': 0,
+            'origin': 'https://www.youtube.com'
         },
         videoId: 'X8Z8okhkjv8',
         events: {
             'onReady': onPlayerReady
         }
     });
-    player3 = new YT.Player('player3', {
+    players[2] = new YT.Player('player3', {
         playerVars: {
             'controls': 0,
             'fs': 0,
@@ -51,7 +87,8 @@ function onYouTubeIframeAPIReady() {
             'playlist': 'M7lc1UVf-VE',
             'modestbranding': 1,
             'showinfo': 0,
-            'rel': 0
+            'rel': 0,
+            'origin': 'https://www.youtube.com'
         },
         videoId: 'M7lc1UVf-VE',
         events: {
@@ -65,6 +102,73 @@ function onPlayerReady(event) {
     event.target.mute();
 }
 
-function stopVideo() {
-    player.stopVideo();
+function initGoogleMaps() {
+    initMap(34.0194, -118.411);
 }
+
+function initMap(latitude, longitude) {
+    const mapProp = {
+        center: new google.maps.LatLng(latitude, longitude),
+        zoom: 10,
+        disableDefaultUI: true
+    };
+    const mapMarkerUrl = 'img/icons/map-marker.svg';
+    const map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    const mapMarkerProp = {
+        position: mapProp.center,
+        map,
+        icon: mapMarkerUrl
+    };
+    const mapMarker = new google.maps.Marker(mapMarkerProp);
+}
+
+function createMapPoint(locationNumber) {
+    for (let i = 0; i < 3; i++) {
+        document.getElementsByClassName("location__name")[i].innerHTML = locations[locationNumber].cityName;
+    }
+
+    stylizeOpenLocation(locations[locationNumber].cityName);
+    initMap(locations[locationNumber].latitude, locations[locationNumber].longitude);
+}
+
+function createIdsToLocations() {
+    locations.map((el, index) => {
+        let element = document.getElementsByClassName("location__item")[index];
+
+        element.id = el.cityName.replace(/ /g, '');
+    });
+}
+
+function stylizeOpenLocation(cityName) {
+    locations.map((el, index) => {
+        let element = document.getElementsByClassName("location__item")[index];
+
+        cityName === el.cityName ?
+            element.style.boxShadow = '20px 20px 50px rgba(0, 0, 0, 0.0758039)' :
+            element.style.boxShadow = 'none';
+    });
+}
+
+document.addEventListener("DOMContentLoaded", createIdsToLocations());
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+window.addEventListener('scroll', function () {
+    if (pageYOffset) {
+        players.map((el) => {
+            el.pauseVideo();
+        });
+    } else {
+        players.map((el) => {
+            el.playVideo();
+        });
+    }
+});
